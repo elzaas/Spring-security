5876f123c307ef574c2c3df615387b8a05ecfa09
Elza <elza.assanova@gmail.com>
2024-05-19 15:18:21 +0600

    basic authentication

---
+++ src/main/java/com/workshhop/elza/ElzaApplication.java
@@ -0,0 +1,13 @@
+package com.workshhop.elza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workshhop/elza/ElzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshhop.elza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
